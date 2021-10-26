0f32aec575e9251cc5913c1eaa1f7f0503fc4edf
Tuan Loc <tuanlocvocong@gmail.com>
2021-10-26 23:40:54 +0700

    update booking movie

--- src/containers/PageNotFound/index.js
+++ src/containers/PageNotFound/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import NavMovie from "../HomeTemplate/HomePage/NavMovie/index";
+import Header from "../HomeTemplate/HomePage/Header";
 
 export default class PageNotFound extends Component {
   render() {
     return (
-      <div>
-        <h3>PageNotFound</h3>
-      </div>
+      <>
+        <Header />
+        <NavMovie />
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+            marginTop: "90px",
+            position: "fixed",
+          }}
+          src="https://1.bp.blogspot.com/-cr0bAHr5IYc/VyIXSsZUycI/AAAAAAAAH9k/nTU427gW8KsjYcrONOim6PIUHlUgB36mgCLcB/s1600/loi-404-cach-khac-phuc-loi-404-cho-website.png"
+          atl="404"
+        />
+      </>
     );
   }
 }
